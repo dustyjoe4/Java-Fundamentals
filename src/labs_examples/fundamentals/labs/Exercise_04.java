8292a931bd2c821f2f3fa750b1ec88f1c48ea431
Dustin Smith <dustinsmith@Dustins-iMac.local>
2021-01-05 16:03:30 -0600

    omplete Example 4 & 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -13,7 +13,24 @@ public class Exercise_04 {
     public static void main(String[] args) {
 
         int i = 1;
+        char letter = 'A';
+        byte three = 3;
+        boolean isEmpty = false;
+        short oneThousand = 1000;
+        long oneMillion = 1000000l;
+        double decimal = 123.45;
+        float bigNumber = 23457773549992003f;
+
+
+
         System.out.println("int i is: " + i);
+        System.out.println(letter);
+        System.out.println(three);
+        System.out.println(isEmpty);
+        System.out.println(oneThousand);
+        System.out.println(oneMillion);
+        System.out.println(decimal);
+        System.out.println(bigNumber);
 
         // write your code below
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,11 +14,17 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int length = str.length();
+        System.out.println(length);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean matchingStrings = str.equalsIgnoreCase(str2);
+        System.out.println(matchingStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String result = str + " " + str2;
+        System.out.println(result);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
